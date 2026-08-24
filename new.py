c13aff12776af89d83102be59cd9338778096799
kaustubhpandey000012 <kaustubhpandey0012@gmail.com>
2026-08-24 11:52:04 +0530

    created a program

---
+++ new.py
@@ -0,0 +1,19 @@
+
+def add(a,b):
+    result = a+b
+    print(result)
+
+def substract(a,b):
+    result = a-b
+    print(result)
+
+def multiply(a,b):
+    result =  a*b
+    print(result)
+
+def divide(a,b):
+    result = a/b
+    print(result)
+
+add(5,4)
+
